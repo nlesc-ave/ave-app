eb42fd6a11633cde4b08b24aaf3580cbc304b220
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2017-05-31 08:09:31 +0200

    Wrapp dev env of app with electron

---
+++ electron-main.js
@@ -0,0 +1,33 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({width: 1100, height: 800})
+
+  // diable menu bar
+  win.setMenu(null);
+
+  win.loadURL('http://localhost:3000')
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
