2675472a15cc2180bdc1cba4802078078ca2ee89
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2017-09-27 19:07:56 +0200

    Inverted ambiguous nucliotide of alternatives toggle

--- src/region/haplotype/components/VariantInfo.tsx
+++ src/region/haplotype/components/VariantInfo.tsx
@@ -37,7 +37,7 @@ export const VariantInfo = ({ variant }: IProps) => {
     )
   })
   let ambi_alt = null
-  if (variant.alt[0] === variant.alt_ambiguous_nucleotide) {
+  if (variant.alt[0] !== variant.alt_ambiguous_nucleotide) {
     ambi_alt = (
       <div>
         Ambiguous nucliotide of alternatives: {variant.alt_ambiguous_nucleotide}
